51ccb63545c7550c182792ce9d7735488bf37fd3
RaufRahman <rauf.rahmancz@gmail.com>
2019-08-04 13:48:41 +0200

    reorganized sensitive data

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 
 // Initialize Firebase
 let config = {
-  apiKey: "AIzaSyBBYeektxnI3SR8Xra3SNihtMD5iZP4iCM",
-  authDomain: "uddok-532a5.firebaseapp.com",
-  databaseURL: "https://uddok-532a5.firebaseio.com",
-  projectId: "uddok-532a5",
-  storageBucket: "uddok-532a5.appspot.com",
-  messagingSenderId: "28546463605"
+  apiKey: '',
+  authDomain: "",
+  databaseURL: '',
+  projectId: "",
+  storageBucket: '',
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 
